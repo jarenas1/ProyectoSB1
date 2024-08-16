f16a6ab5d040c776d8cc7c41d07885b141cc0e1b
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-16 15:06:07 -0500

    users-inserted-commandLineRunner

--- src/main/java/com/riwi/first_web_aplication/FirstWebAplicationApplication.java
+++ src/main/java/com/riwi/first_web_aplication/FirstWebAplicationApplication.java
@@ -1,13 +1,39 @@
 package com.riwi.first_web_aplication;
 
+import com.riwi.first_web_aplication.entities.PersonEntity;
+import com.riwi.first_web_aplication.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class FirstWebAplicationApplication {
+public class FirstWebAplicationApplication implements CommandLineRunner {//Para que nos permita correr las cosas aca
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstWebAplicationApplication.class, args);
 	}
 
+	//AHORA VAMOS A AÑADIR USUARIOS A LA BASE DE DATOS POR MEDIO DE LAS COSAS QIE EXTENDIMOS EN EL RESPOSITORIO
+	//Instanciamos el repository para poder acceder a los metodos etendidos a esta
+	@Autowired
+	 private PersonRepository personRepository;
+
+
+	//CREAMOS ESTE METODO PARA PODER CORRER UN BEAN, EN ESTE CASO EL REPOSITORY
+	@Override
+	public void run(String... args)throws Exception{
+		personRepository.save(new PersonEntity(1L,"Juan",18));
+		personRepository.save(new PersonEntity(2L,"Andres",38));
+
+		System.out.println("Cantidad de registros en la tabla por medio de .count que viene de repository: "+ personRepository.count());
+
+		System.out.println("Lista de personas con el metodo findAll()");
+		List<PersonEntity> persons = personRepository.findAll(); //AHORA SOLO ITERAMOS
+		persons.forEach(person -> System.out.println("name "+ person.getName()));
+
+	}
+//LO UNICO QUE TENEMOS QUE HACER ES LLAMAR AL METODO SAVE DEL REPOSITORY PARA GUARDAR TODO AUTOMATICAMENTE, SIN TNEER QUE CREAR NADA COMO EN JDBC
 }
--- src/main/java/com/riwi/first_web_aplication/entities/PersonEntity.java
+++ src/main/java/com/riwi/first_web_aplication/entities/PersonEntity.java
@@ -3,17 +3,27 @@ package com.riwi.first_web_aplication.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity //INDICAMOS QUE ES UNA ENTIDAD PARA QUE ME PUEDA PERMITIR MAPEARLO
 
 @Table(name = "persons") //Esto indica el nombre que va a tener la tabla cuando se cree si no ponemos nada se creara con el nombre de la clase
-public class PersonEntity {
+//CREAR GETTERS, CONSTRUCTOR Y SETTER ESTO VIENE DE LOMBOOK
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
+public class PersonEntity {
     //AÑADIMOS LOS ATRIBUTOS DE ESTA ENTIDAD
 
-    @Id //Como esto va a ser mapeado y convertido en una entidad, debemos indicar una PK, y para esto le ponemos @id al id
+    @Id
+    //Como esto va a ser mapeado y convertido en una entidad, debemos indicar una PK, y para esto le ponemos @id al id
     private Long id;
 
     private String name;
+
     private int age;
+
 }
